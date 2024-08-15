edce0006f8f76575a1a801cfdb316bb386b91783
Чекаловец Александр Валерьевич <a.chekalovets@ZOLOTO55.LAN>
2024-08-15 17:09:11 +0600

    add manual transcription

--- Front/static/js/index.js
+++ Front/static/js/index.js
@@ -31,7 +31,7 @@ async function saveTextToFile(e) {
     const response = await fetch(
         "http://172.20.3.134:5002/save_text", {
             method: "POST",
-            body: JSON.stringify(file_data)
+            body: file_data
         })
     if (response.ok) {
         console.log(`Текст записи ${e.target.id} успешно сохранен!`)
